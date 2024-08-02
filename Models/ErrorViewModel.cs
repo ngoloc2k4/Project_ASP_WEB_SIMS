9114ed3e5e540603085560c5f73d664acdcb703a
Ngô Kim Lộc <kim.loc.ngo.0011@gmail.com>
2024-08-02 15:43:00 +0700

    Create project in C#

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_ASP_WEB_SIMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
